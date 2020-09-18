0ed26ec75c3ba51f29e97a72b8d93c2e507d0b80
ANALUGARITA <analugarita@gmail.com>
2020-09-17 18:00:12 -0600

    Programa para leer v2

--- readFile.py
+++ readFile.py
@@ -1,3 +1,5 @@
+# Recorrido e impresi'on de las l'ineas de un archivo de texto
 with open("maravillas_antiguas.csv", "r") as archivo:
     for linea in archivo:
-        print(linea, end='')
+        if (linea.find('Grecia') != -1):
+            print(linea, end='')        
